814aea8d9a929a0bde70cfafc04583b0bdd4f9b7
Will B Chang <will.b.chang@gmail.com>
2019-10-26 17:20:27 +0800

    fix: can't save while loading tab

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -36,7 +36,7 @@ function get(tab) {
   return {
     [Date.now()]: {
       url: tab.url,
-      title: tab.title || url,
+      title: tab.title || tab.url,
       favIconUrl: tab.favIconUrl || 'src/images/32x32gray.png',
     }
   }
