075fa7f997ec7225596eb9851ce0774420d919b6
Will B Chang <will.b.chang@gmail.com>
2019-10-28 14:43:35 +0800

    fix: Use Object.defindeProperty to resolve confict with jQuery

--- modules/tab.prototype.js
+++ modules/tab.prototype.js
@@ -1,22 +1,31 @@
 import * as tabs from "./tabs.js"
 
-Object.prototype.isEmpty = function () {
-  return this.url === 'chrome://newtab/'
-}
+// Use Object.defindeProperty to resolve confict with jQuery
+// https://stackoverflow.com/a/21730003/9984029
+Object.defineProperty(Object.prototype, 'isEmpty', {
+  value: function () { return this.url === 'chrome://newtab/' },
+  enumerable: false
+})
 
-Object.prototype.getInfo = function () {
-  return {
-    [Date.now()]: {
-      url: this.url,
-      title: this.title || this.url,
-      favIconUrl: this.favIconUrl || '../images/32x32gray.png',
+Object.defineProperty(Object.prototype, 'getInfo', {
+  value: function () {
+    return {
+      [Date.now()]: {
+        url: this.url,
+        title: this.title || this.url,
+        favIconUrl: this.favIconUrl || '../images/32x32gray.png',
+      }
     }
-  }
-}
+  },
+  enumerable: false
+})
 
-Object.prototype.setEmptyOrRemove = function () {
-  // query all tabs with {}
-  tabs.query({}, aTabs => {
-    aTabs.length === 1 ? tabs.empty(this) : tabs.remove(this)
-  });
-}
+Object.defineProperty(Object.prototype, 'setEmptyOrRemove', {
+  value: function () {
+    // query all tabs with {}
+    tabs.query({}, aTabs => {
+      aTabs.length === 1 ? tabs.empty(this) : tabs.remove(this)
+    })
+  },
+  enumerable: false
+})
