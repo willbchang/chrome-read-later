3b4a19ebea6ca392a21531c544ba12bc3c2e2e82
Will B Chang <will.b.chang@gmail.com>
2019-10-28 10:48:03 +0800

    fix: can't save url as title when selection is url

--- modules/page.js
+++ modules/page.js
@@ -2,7 +2,7 @@ export function get(info, tab) {
   return {
     [Date.now()]: {
       url: info.linkUrl,
-      title: info.selectionText || url,
+      title: info.selectionText || info.linkUrl,
       favIconUrl: tab.favIconUrl || '../images/32x32orange.png',
     }
   }
