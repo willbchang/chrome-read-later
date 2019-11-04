47fa7cb752e746252216e5fbd466c24822676c49
Will B Chang <will.b.chang@gmail.com>
2019-11-04 16:04:22 +0800

    popup.append() change <sub> to <span>

--- popup.js
+++ popup.js
@@ -22,7 +22,7 @@ function appendReadingListToHtml() {
       <li id=${page.date}>
         <img src="${page.favIconUrl}">
         <a href="${page.url}" target="_blank">${page.title}</a>
-        <sub>${page.position}</sub>
+        <span>${page.position}</span>
       </li>
     `)
   }
