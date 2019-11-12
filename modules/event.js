09dfeae493847baa22b239abcf4cc285f4f567e2
Will B Chang <will.b.chang@gmail.com>
2019-11-12 10:44:55 +0800

    refactor: add event.onHover()

--- modules/event.js
+++ modules/event.js
@@ -1,3 +1,7 @@
 export function onClick(selector, callback) {
   $('ul').on('click', selector, callback)
 }
+
+export function onHover(selector, onEnter, onLeave) {
+  $(document).on({ mouseenter: onEnter, mouseleave: onLeave }, selector)
+}
