d5e3f430da295c86148db05b97f9c77d97ec57bd
Will B Chang <will.b.chang@gmail.com>
2019-11-12 11:48:28 +0800

    refactor: content.js rename parameter send to sendResponse

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
-chrome.runtime.onMessage.addListener((message, sender, send) => {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.info === 'get page position') {
     const position = {}
     position.scrollTop = document.documentElement.scrollTop
     position.scrollBottom = window.scrollY + window.innerHeight
     position.scrollHeight = document.documentElement.scrollHeight
-    send(position)
+    sendResponse(position)
   }
 
   if (message.scrollTop) {
