480e01dcc3717d0abb8c440d540d951ea1430944
Will B Chang <will.b.chang@gmail.com>
2019-11-13 22:50:44 +0800

    refactor: rewrite callback in extensino.onCommand() with async await

--- background.js
+++ background.js
@@ -2,27 +2,15 @@ import * as extension from './modules/extension.js'
 import * as storage from './modules/storage.js'
 import * as tabs from './modules/tabs.js'
 
-// Use Promise for onCommand listener will cause only listen once.
-extension.onCommand(() => {
-  // https://stackoverflow.com/questions/28250680/how-do-i-access-previous-promise-results-in-a-then-chain
-  // I choose to use variable to maintain code readability.
-  let tab
-  tabs
-    .current()
-    .then(aTab => {
-      tab = aTab
-      // Break the promise chain when the tab is empty.
-      // https://stackoverflow.com/a/45339587/9984029
-      if (tabs.isEmpty(tab)) return { then: () => {} }
-      return tabs.sendMessage(tab.id, { info: 'get page position' })
-    })
-    .then(position => {
-      storage.setPage(tab, position)
-      return tabs.query({})
-    })
-    .then(allTabs => {
-      allTabs.length === 1 ? tabs.empty(tab) : tabs.remove(tab)
-    })
+extension.onCommand(async () => {
+  const tab = await tabs.current()
+  if (tabs.isEmpty(tab)) return
+
+  const position = await tabs.sendMessage(tab.id, { info: 'get page position' })
+  storage.setPage(tab, position)
+
+  const allTabs = await tabs.query({})
+  allTabs.length === 1 ? tabs.empty(tab) : tabs.remove(tab)
 })
 
 extension.onMessage(message => {
