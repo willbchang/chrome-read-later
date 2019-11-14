475ff53852c46714456eb1d0a91d83de25d172f5
Will B Chang <will.b.chang@gmail.com>
2019-11-14 16:42:12 +0800

    doc: add comments for event.js

--- modules/event.js
+++ modules/event.js
@@ -1,7 +1,10 @@
+// https://api.jquery.com/on/
 export function onClick(selector, callback) {
   $('ul').on('click', selector, callback)
 }
 
+// https://api.jquery.com/hover/
+// https://stackoverflow.com/a/9827114/9984029
 export function onHover(selector, onEnter, onLeave) {
   $('ul').on({ mouseenter: onEnter, mouseleave: onLeave }, selector)
 }
