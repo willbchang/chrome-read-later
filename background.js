9f5f503bb41634db7348787cd406a79500365564
Will B Chang <will.b.chang@gmail.com>
2019-11-20 12:05:12 +0800

    feature: only save tab info when tab is loading (close #38)

--- background.js
+++ background.js
@@ -3,13 +3,17 @@ import * as storage from './modules/storage.js'
 import * as tabs from './modules/tabs.js'
 
 extension.onCommand(async () => {
+  const allTabs = await tabs.all()
   const tab = await tabs.current()
   if (tabs.isEmpty(tab)) return
+  
+  if (tab.status !== 'complete') {
+    storage.setPage(tab)
+  } else {
+    const position = await tabs.sendMessage(tab.id, { info: 'get page position' })
+    storage.setPage(tab, position)
+  }
 
-  const position = await tabs.sendMessage(tab.id, { info: 'get page position' })
-  storage.setPage(tab, position)
-
-  const allTabs = await tabs.all()
   allTabs.length === 1 ? tabs.empty() : tabs.remove(tab)
 })
 
@@ -19,6 +23,7 @@ extension.onMessage(async message => {
 
   const position = await storage.getPosition(message.url)
   storage.remove(message.url)
+  if (!position.scrollTop) return
 
   // Use raw sendMessage to avoid the error message below:
   // The message port closed before a response was received.
