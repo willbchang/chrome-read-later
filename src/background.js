ff89eac4f3debad37ec2451f898fb36f3972f741
Will B Chang <will.b.chang@gmail.com>
2019-11-23 13:40:42 +0800

    rewrite background.onCommand() with if else

--- src/background.js
+++ src/background.js
@@ -5,10 +5,9 @@ import * as tabs from '../modules/tabs.js'
 extension.onCommand(async () => {
   const tab = await tabs.queryCurrent()
   if (tabs.isEmpty(tab)) return
-
-  if (tab.status !== 'complete' || tab.url.slice(0, 4) !== 'http') {
-    storage.setPage(tab)
-  } else {
+  else if (tab.url.slice(0, 4) !== 'http') storage.setPage(tab)
+  else if (tab.status !== 'complete') storage.setPage(tab)
+  else {
     const position = await tabs.sendMessage(tab.id, { info: 'get position' })
     storage.setPage(tab, position)
   }
