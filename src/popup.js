759c87638ce3058b103d4ee007262323bdbbc4c2
Will B Chang <will.b.chang@gmail.com>
2019-11-26 17:14:55 +0800

    docs: update comments

--- src/popup.js
+++ src/popup.js
@@ -20,7 +20,7 @@ function sendUrlToBackground(e) {
   //  - Check if current tab is empty: tabs.isEmpty()
   //  - Add tab loading status listener: tabs.onComplete()
   //  - Get saved page scroll position: storage.getPosition()
-  //  - Send position to content.js: chrome.tabs.sendMessage()
+  //  - Send position to content.js: tabs.sendMessage()
   //  - Remove this item in storage: storage.remove()
   e.preventDefault()
   // Send clicked url as message to background.
@@ -28,7 +28,8 @@ function sendUrlToBackground(e) {
   // popup.html will disappeared after clicking the link,
   // thus the listener in popup.js would be interrupted.
   // `e.target.parentNode.href` is for the case when url is title,
-  // to break the long 'word', the whole title is contained by <span>
+  // to break the long 'word', the whole title is contained by <span>.
+  // Please check getReadingListFrom() and breakLongWord() in data.js
   extension.sendMessage({url: e.target.href || e.target.parentNode.href})
   // Close popup.html when loading in current tab,
   // which also means the current tab is empty.
