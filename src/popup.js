b1fe26295a74c0a0714c9a33d4b8ce0cd68aa3b2
Will B Chang <will.b.chang@gmail.com>
2019-12-02 12:23:27 +0800

    refactor: rename parameter e to event

--- src/popup.js
+++ src/popup.js
@@ -14,7 +14,7 @@ async function initReadingList() {
   pages.map(page => $('ul').append(createReadingItem(page)))
 }
 
-function sendUrlToBackground(e) {
+function sendUrlToBackground(event) {
   // Disable the default <a> tag action,
   // because there are some actions need to be run:
   //  - Check if current tab is empty: tabs.isEmpty()
@@ -22,34 +22,34 @@ function sendUrlToBackground(e) {
   //  - Get saved page scroll position: storage.getPosition()
   //  - Send position to content.js: tabs.sendMessage()
   //  - Remove this item in storage: storage.remove()
-  e.preventDefault()
+  event.preventDefault()
   // Send clicked url as message to background.
   // Because tabs.onComplete() is a live listener,
   // popup.html will disappeared after clicking the link,
   // thus the listener in popup.js would be interrupted.
-  // `e.target.parentNode.href` is for the case when url is title,
+  // `event.target.parentNode.href` is for the case when url is title,
   // to break the long 'word', the whole title is contained by <span>.
   // Please check getReadingListFrom() and breakLongWord() in data.mjs
-  extension.sendMessage({url: e.target.href || e.target.parentNode.href})
+  extension.sendMessage({url: event.target.href || event.target.parentNode.href})
   // Close popup.html when loading in current tab,
   // because tabs.update() won't close popup.html automatically,
   // tabs.create() does.
   window.close()
 }
 
-function showDeleteIcon(e) {
-  localStorage.setItem('src', $(e.target).attr('src'))
-  $(e.target).attr('src', '../images/32x32delete.png')
+function showDeleteIcon(event) {
+  localStorage.setItem('src', $(event.target).attr('src'))
+  $(event.target).attr('src', '../images/32x32delete.png')
 }
 
-function showFavIcon(e) {
-  $(e.target).attr('src', localStorage.getItem('src'))
+function showFavIcon(event) {
+  $(event.target).attr('src', localStorage.getItem('src'))
   localStorage.clear()
 }
 
-function removeItem(e) {
-  // e.target is <img>, the parentNode is <li>
-  $(e.target.parentNode).remove()
+function removeItem(event) {
+  // event.target is <img>, the parentNode is <li>
+  $(event.target.parentNode).remove()
   // The next sibling of <img> is <a>
-  storage.remove(e.target.nextElementSibling.href)
+  storage.remove(event.target.nextElementSibling.href)
 }
