5790699b9b4bfdc75672f7d366aa7855a0bfc0a3
Will B Chang <will.b.chang@gmail.com>
2019-12-02 23:23:15 +0800

    refactor: data.mjs move breakLongWord() to getTitle()

--- modules/data.mjs
+++ modules/data.mjs
@@ -75,18 +75,19 @@ export function getReadingItem(page) {
 
   function getTitle() {
     return page.title.split(' ').map(breakLongWord).join(' ')
+
+    // This function is especially for but not limited to
+    // the case: url is title. The url is treated as
+    // one word and not in the dictionary, so it can't be
+    // broken automatically with hyphen.
+    // This is also a word: tester-testABCDEFGHI?title
+    function breakLongWord(word) {
+      if (word.isMaxLength)
+        return `<span style="word-break: break-all">${word}</span>`
+      return word
+    }
   }
 
-  // This function is especially for but not limited to
-  // the case: url is title. The url is treated as
-  // one word and not in the dictionary, so it can't be
-  // broken automatically with hyphen.
-  // This is also a word: tester-testABCDEFGHI?title
-  function breakLongWord(word) {
-    if (word.isMaxLength)
-      return `<span style="word-break: break-all">${word}</span>`
-    return word
-  }
 
   function getScrollPercent() {
     // Get scroll percent when page.scrollTop doesn't exist or the value is zero.
