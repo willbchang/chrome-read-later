100ba381319af6de163db275c84c796a2c755291
Will B Chang <will.b.chang@gmail.com>
2019-12-02 12:21:42 +0800

    refactor: extract check word length to String.prototype.isMaxLength

--- modules/prototype.mjs
+++ modules/prototype.mjs
@@ -6,3 +6,20 @@ Object.defineProperty(Object.prototype, 'isEmpty', {
   },
   enumerable: false
 })
+
+Object.defineProperty(String.prototype, 'isMaxLength', {
+  value: function () {
+    // ul: 300px(width) - 15px*2(padding) = 270px(content width)
+    // favicon:  16px(content width) + 5px*2(padding) = 26px(width)
+    // li: 270px - 26px = 244px
+    // letter-spacing: 0.4px
+    // body: font-size: 14px
+    // Check: https://jsfiddle.net/2750Lujs
+    // The largest letter W has 13px(NOTE: some fonts are not equal width)
+    // 244px / 13.4px(letter width) ≈ 18 letters
+    // Assume a word is made by W, if the length is larger than 244px,
+    // the favicon and the word won't be in the same line,
+    // or the word in title will ugly break the line.
+    return this.length === 18
+  }
+})
