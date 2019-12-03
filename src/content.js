9af2e9056f4a7f598bb8046c3709a38fd6ea0127
Will B Chang <will.b.chang@gmail.com>
2019-12-03 22:59:56 +0800

    refactor: extract set page position to setPagePosition()

--- src/content.js
+++ src/content.js
@@ -4,16 +4,19 @@
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.info === 'get position')
       sendResponse(data.getPagePosition())
-
-    if (message.scrollTop)
-      window.scrollTo({
-        top: message.scrollTop,
-        behavior: 'smooth'
-      })
+    setPagePosition(message)
   })
 })()
 
 async function importModule(url) {
   const src = chrome.runtime.getURL(url)
   return await import(src)
+}
+
+function setPagePosition(message) {
+  if (message.scrollTop)
+    window.scrollTo({
+      top: message.scrollTop,
+      behavior: 'smooth'
+    })
 }
