3528d6771c06ed84aabb0e6c2e89a8e7dbc94cba
Will B Chang <will.b.chang@gmail.com>
2019-12-03 22:46:56 +0800

    fix: use dynamic import to resolve import in content.js

--- src/content.js
+++ src/content.js
@@ -1,12 +1,16 @@
-import {getPagePosition} from '../modules/data.mjs'
+(async () => {
+  const src = chrome.runtime.getURL('modules/data.mjs')
+  const data = await import(src)
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.info === 'get position')
-    sendResponse(getPagePosition())
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.info === 'get position')
+      sendResponse(data.getPagePosition())
 
-  if (message.scrollTop)
-    window.scrollTo({
-      top: message.scrollTop,
-      behavior: 'smooth'
-    })
-})
+    if (message.scrollTop)
+      window.scrollTo({
+        top: message.scrollTop,
+        behavior: 'smooth'
+      })
+  })
+})()
+
