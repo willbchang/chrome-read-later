e1df60240f4c8a75d2aac87cc14eb03bfbb145c7
Will B Chang <will.b.chang@gmail.com>
2019-12-03 22:34:29 +0800

    refactor: move getPagePosition() to data.mjs

--- modules/data.mjs
+++ modules/data.mjs
@@ -1,4 +1,29 @@
 import './prototype.mjs'
+
+// Use scrollBottom to calculate scrollPercent to avoid the situation:
+// Scroll page to the bottom, but the percent is not 100%.
+export function getPagePosition() {
+  // In popup.js, there is a `if` statement to check if the scrollTop is 0,
+  // if it is 0, the scrollPercent won't add to popup.html.
+  // Do not worry this situation: scrollTop: 0, scrollPercent: 100%
+  return {
+    scrollTop: getScrollTop(),
+    scrollBottom: getScrollBottom(),
+    scrollHeight: getScrollHeight(),
+  }
+
+  function getScrollTop() {
+    return document.documentElement.scrollTop
+  }
+
+  function getScrollBottom() {
+    return window.scrollY + window.innerHeight
+  }
+
+  function getScrollHeight() {
+    return document.documentElement.scrollHeight
+  }
+}
 // https://mdn.io/object.spread
 // https://git.io/Je6Aq
 // https://mdn.io/default_parameters
--- src/content.js
+++ src/content.js
@@ -1,3 +1,5 @@
+import {getPagePosition} from '../modules/data.mjs'
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.info === 'get position')
     sendResponse(getPagePosition())
@@ -8,28 +10,3 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       behavior: 'smooth'
     })
 })
-
-// Use scrollBottom to calculate scrollPercent to avoid the situation: 
-// Scroll page to the bottom, but the percent is not 100%.
-function getPagePosition() {
-  // In popup.js, there is a `if` statement to check if the scrollTop is 0,
-  // if it is 0, the scrollPercent won't add to popup.html.
-  // Do not worry this situation: scrollTop: 0, scrollPercent: 100%
-  return {
-    scrollTop: getScrollTop(),
-    scrollBottom: getScrollBottom(),
-    scrollHeight: getScrollHeight(),
-  }
-
-  function getScrollTop() {
-    return document.documentElement.scrollTop
-  }
-
-  function getScrollBottom() {
-    return window.scrollY + window.innerHeight
-  }
-
-  function getScrollHeight() {
-    return document.documentElement.scrollHeight
-  }
-}
