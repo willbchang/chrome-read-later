cc8ee6c3fcf72b31d29f38b598a63b8d50079215
Will B Chang <will.b.chang@gmail.com>
2019-12-05 08:33:57 +0800

    refactor: move data.getPageInfo() to storage.mjs

--- modules/storage.mjs
+++ modules/storage.mjs
@@ -1,3 +1,5 @@
+import './prototype.mjs'
+
 // For chrome.storage functions:
 // https://developer.chrome.com/extensions/storage
 export function remove(url) {
@@ -26,4 +28,63 @@ export async function getPagePosition(url) {
 
 export function set(page) {
   chrome.storage.sync.set(page)
+}
+
+// https://mdn.io/object.spread
+// https://git.io/Je6Aq
+// https://mdn.io/default_parameters
+// Use object as parameter to get optional parameter.
+// Set default empty value to 'selection' to avoid
+// Cannot read property of undefined
+export function getPageInfo({tab, position = {}, selection = {}}) {
+// https://mdn.io/computed_property_names
+  return {
+    [getUrl()]: {
+      url: getUrl(),
+      title: getTitle(),
+      favIconUrl: getFavIconUrl(),
+      date: getDate(),
+      scrollTop: getScrollTop(),
+      scrollPercent: getScrollPercent(),
+    }
+  }
+
+  function getUrl() {
+    return selection.linkUrl || tab.url
+  }
+
+  function getTitle() {
+    if (selection.isEmpty()) return tab.title || tab.url
+    // TODO: Will fetch page info(title, favicon) via url in later version.
+    // Select item in google search will also select its url.
+    if (tab.url.includes('://www.google.'))
+      return filterUrl(selection.selectionText)
+    return selection.selectionText || selection.url
+
+    function filterUrl(text) {
+      // FIX: Cannot avoid http:// in google search,
+      // the http:// doesn't reveal. Needs to use url regex.
+      return text.split('https://')[0]
+    }
+  }
+
+  function getFavIconUrl() {
+    return tab.favIconUrl || '../images/32x32gray.png'
+  }
+
+  function getDate() {
+    return Date.now()
+  }
+
+  function getScrollTop() {
+    return position.scrollTop || 0
+  }
+
+  function getScrollPercent() {
+    return percent(position.scrollBottom / position.scrollHeight)
+
+    function percent(num = 0) {
+      return Math.floor(num * 100) + '%'
+    }
+  }
 }
