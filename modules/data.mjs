c9442b2543773561ad864b088840eaa519f815fb
Will B Chang <will.b.chang@gmail.com>
2019-12-06 16:20:46 +0800

    refactor: move page.getInfo() to data.getInfo()

--- modules/data.mjs
+++ modules/data.mjs
@@ -1,5 +1,64 @@
 import './prototype.mjs'
 
+// https://git.io/Je6Aq
+// https://mdn.io/object.spread
+// https://mdn.io/default_parameters
+// https://mdn.io/computed_property_names
+// Use object as parameter to get optional parameter.
+// Set default empty value to 'selection' to avoid
+// Cannot read property of undefined
+export function getInfo({tab, position = {}, selectionText, linkUrl}) {
+  return {
+    [getUrl]: {
+      url: getUrl(),
+      title: getTitle(),
+      favIconUrl: getFavIconUrl(),
+      date: getDate(),
+      scrollTop: getScrollTop(),
+      scrollPercent: getScrollPercent(),
+    }
+  }
+
+  function getUrl() {
+    return linkUrl || tab.url
+  }
+
+  function getTitle() {
+    if (!selectionText) return tab.title || tab.url
+    // TODO: Will fetch page info(title, favicon) via url in later version.
+    // Select item in google search will also select its url.
+    if (tab.url.includes('://www.google.'))
+      return filterUrl(selectionText)
+    return selectionText || linkUrl
+
+    function filterUrl(text) {
+      // FIX: Cannot avoid http:// in google search,
+      // the http:// doesn't reveal. Needs to use url regex.
+      return text.split('https://')[0]
+    }
+  }
+
+  function getFavIconUrl() {
+    return tab.favIconUrl || '../images/32x32gray.png'
+  }
+
+  function getDate() {
+    return Date.now()
+  }
+
+  function getScrollTop() {
+    return position.scrollTop || 0
+  }
+
+  function getScrollPercent() {
+    return percent(position.scrollBottom / position.scrollHeight)
+
+    function percent(num) {
+      return (Math.floor(num * 100) || 0) + '%'
+    }
+  }
+}
+
 export function renderHtmlText(page) {
   return ` 
       <li id=${page.date}>
