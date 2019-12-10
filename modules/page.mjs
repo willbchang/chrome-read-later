ab31139537b3ff6b552ab39de2c033a42fc50119
Will B Chang <will.b.chang@gmail.com>
2019-12-10 10:30:57 +0800

    refactor: rewrite page.mjs with class

--- modules/page.mjs
+++ modules/page.mjs
@@ -1,37 +1,51 @@
 // Use scrollBottom to calculate scrollPercent to avoid the situation:
 // Scroll page to the bottom, but the percent is not 100%.
+class ScrollPosition {
+  constructor(position) {
+    this.position = position
+  }
+
+  get scrollTop() {
+    return document.documentElement.scrollTop
+  }
+
+  get scrollBottom() {
+    return window.scrollY + window.innerHeight
+  }
+
+  get scrollHeight() {
+    return document.documentElement.scrollHeight
+  }
+
+  get dynamicScrollTop() {
+    if (this.position.scrollHeight)
+      return this.position.scrollTop / this.position.scrollHeight * this.scrollHeight
+    return this.scrollTop
+  }
+
+  scrollTo(scrollTop) {
+    window.scrollTo({
+      top: scrollTop,
+      behavior: 'smooth'
+    })
+  }
+}
+
 export function getScrollPosition() {
   // In popup.js, there is a `if` statement to check if the scrollTop is 0,
   // if it is 0, the scrollPercent won't add to popup.html.
   // Do not worry this situation: scrollTop: 0, scrollPercent: 100%
+  const scrollPosition = new ScrollPosition({})
   return {
-    scrollTop: getScrollTop(),
-    scrollBottom: getScrollBottom(),
-    scrollHeight: getScrollHeight(),
-  }
-
-  function getScrollTop() {
-    return document.documentElement.scrollTop
-  }
-
-  function getScrollBottom() {
-    return window.scrollY + window.innerHeight
-  }
-
-  function getScrollHeight() {
-    return document.documentElement.scrollHeight
+    scrollTop: scrollPosition.scrollTop,
+    scrollBottom: scrollPosition.scrollBottom,
+    scrollHeight: scrollPosition.scrollHeight,
   }
 }
 
-export function setScrollPosition({scrollTop, scrollHeight}) {
-  window.scrollTo({
-    top: getCurrentScrollTop(),
-    behavior: 'smooth'
-  })
-
-  function getCurrentScrollTop() {
-    if (scrollHeight)
-      return scrollTop / scrollHeight * document.documentElement.scrollHeight
-    return scrollTop
-  }
+export function setScrollPosition(position) {
+  const scrollPosition = new ScrollPosition(position)
+  scrollPosition.scrollTo(scrollPosition.dynamicScrollTop)
 }
+
+
