bf010326c1afd758bc792e66dde99d0b4b2998a4
Will B Chang <will.b.chang@gmail.com>
2020-04-26 14:56:51 +0800

    fix: encode page.title characters with he.js

--- feat_popup/htmlRender.mjs
+++ feat_popup/htmlRender.mjs
@@ -1,14 +1,19 @@
 export function renderListFrom(page) {
   return ` 
-      <li id=${page.date} title="${page.title}\n${page.url}" tabindex="1">
+      <li id=${page.date} title="${encodeTitle()}\n${page.url}" tabindex="1">
         <img src="${page.favIconUrl}" alt="">
-        <a href="${page.url}" ${getTitleColor()} tabindex="-1">${page.title}</a>
+        <a href="${page.url}" ${getTitleColor()} tabindex="-1">${encodeTitle()}</a>
         ${getScrollPercent()}
       </li>
     `
 
   function getTitleColor() {
     return page.url === page.title ? 'style="color: gray"' : ''
+  }
+
+  function encodeTitle() {
+    // eslint-disable-next-line no-undef
+    return he.encode(page.title)
   }
 
   function getScrollPercent() {
