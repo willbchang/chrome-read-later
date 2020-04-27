fe45f4a6a2535ede0fd2612971982642c80ce6ff
Will B Chang <will.b.chang@gmail.com>
2020-04-27 15:43:21 +0800

    refactor: extract click and keydown action to action.js

--- feat_popup/index.js
+++ feat_popup/index.js
@@ -1,7 +1,6 @@
 import * as html from './htmlRender.mjs'
-import * as extension from '../modules_chrome/runtime.mjs'
 import * as storage from '../modules_chrome/storage.mjs'
-
+import * as action from './action.js'
 
 (async () => {
   // Init reading list
@@ -9,65 +8,11 @@ import * as storage from '../modules_chrome/storage.mjs'
   const pages = await storage.sortByLatest()
   pages.map(page => ul.append(html.renderListFrom(page)))
 
-  // Listen mouse and keyboard events
+  // Listen mouse and keys events
   ul.on({mouseenter: showDeleteIcon, mouseleave: showFavIcon}, 'img')
-    .on('click', performAction)
-    .on('keydown', listenOnKeyboard)
+    .on('click', action.click)
+    .on('keydown', action.keydown)
 })()
-
-function performAction(event) {
-  // https://devdocs.io/jquery/event.preventdefault
-  event.preventDefault()
-  // Get and send reading item's url as message to background,
-  // because they are async/await functions,
-  // index.html will disappear after clicking the link,
-  // thus index.js will be interrupted.
-
-  if (event.target.tagName === 'IMG') return removeReadingItem(event)
-  sendUrlToBackground(event)
-  // Close index.html when loading in current tab.
-  // Update current tab won't close index.html automatically,
-  // but create a new tab does.
-  // window.close()
-}
-
-function listenOnKeyboard(event) {
-  const keyboardActions = {
-    Enter: () => sendUrlToBackground(event),
-    Backspace: () => removeReadingItem(event),
-  }
-
-  try {
-    keyboardActions[event.key]()
-  } catch (e) {
-    console.log('Catch default keyboard action: ', event.key)
-  }
-}
-
-function sendUrlToBackground(event) {
-  const urlStore = {
-    A: () => event.target.href,
-    LI: () => event.target.childNodes[3].href,
-    SPAN: () => event.target.previousSibling.previousSibling.href,
-  }
-
-  extension.sendMessage({url: urlStore[event.target.tagName]()})
-}
-
-function removeReadingItem(event) {
-  const urlStore = {
-    LI: () => event.target.childNodes[3].href,
-    IMG: () => event.target.nextElementSibling.href
-  }
-
-  const removeActions = {
-    LI: () => event.target.remove(),
-    IMG: () => event.target.parentNode.remove(),
-  }
-
-  storage.remove(urlStore[event.target.tagName]())
-  removeActions[event.target.tagName]()
-}
 
 function showDeleteIcon(event) {
   localStorage.setItem('src', event.target.src)
