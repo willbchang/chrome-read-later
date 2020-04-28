0908ecdab45111d9c3e36cd2d6338aeee27fc701
Will B Chang <will.b.chang@gmail.com>
2020-04-28 11:47:44 +0800

    feat: add basic vim, meta and alt key bindings

--- feat_popup/filter.js
+++ feat_popup/filter.js
@@ -14,3 +14,19 @@ export const remove = target => {
     IMG: () => target.parentNode.remove(),
   }[target.tagName]()
 }
+
+export const key = ({key, metaKey, altKey}, lastKey) => {
+  return {
+    Enter: () => metaKey ? 'Meta + Enter' : altKey ? 'Alt + Enter' : 'Enter',
+    Backspace: () => 'Backspace',
+    z: () => metaKey ? 'Command + z' : 'z',
+    j: () => 'j',
+    k: () => 'k',
+    g: () => lastKey === 'g' ? 'gg' : 'g',
+    G: () => 'G',
+    d: () => lastKey === 'd' ? 'dd' : 'd',
+    u: () => 'u',
+    y: () => lastKey === 'y' ? 'yy' : 'y',
+    p: () => 'p',
+  }[key]()
+}
