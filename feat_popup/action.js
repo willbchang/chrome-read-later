9fbade0325df6c6686b6ae1abccd6702ac7ef777
Will B Chang <will.b.chang@gmail.com>
2020-04-28 10:00:32 +0800

    refactor: rename dom.js to filter.js

--- feat_popup/action.js
+++ feat_popup/action.js
@@ -1,14 +1,14 @@
 import * as extension from '../modules_chrome/runtime.mjs'
 import * as storage from '../modules_chrome/storage.mjs'
-import * as dom from './dom.js'
+import * as filter from './filter.js'
 
 export const remove = target => {
-  const url = dom.url(target)
-  dom.remove(target)
+  const url = filter.url(target)
+  filter.remove(target)
   storage.remove(url)
 }
 
 export const open = target => {
-  const url = dom.url(target)
+  const url = filter.url(target)
   extension.sendMessage({url})
 }
