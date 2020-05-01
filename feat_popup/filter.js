7287ed39b44899cef3522ed0ebf174cc6a32907e
Will B Chang <will.b.chang@gmail.com>
2020-05-01 10:06:05 +0800

    style: align dictionary key value

--- feat_popup/filter.js
+++ feat_popup/filter.js
@@ -2,19 +2,19 @@ import * as action from './action.js'
 
 export const url = target => {
   return {
-    A: () => target.href,
-    LI: () => target.childNodes[3].href,
+    A:    () => target.href,
+    LI:   () => target.childNodes[3].href,
     SPAN: () => target.previousSibling.previousSibling.href,
-    IMG: () => target.nextElementSibling.href,
+    IMG:  () => target.nextElementSibling.href,
   }[target.tagName]()
 }
 
 
 export const element = target => {
   return {
-    LI: () => $(target),
-    IMG: () => $(target.parentNode),
-    A: () => $(target.parentNode),
+    LI:   () => $(target),
+    IMG:  () => $(target.parentNode),
+    A:    () => $(target.parentNode),
     SPAN: () => $(target.parentNode),
   }[target.tagName]()
 }
@@ -22,19 +22,19 @@ export const element = target => {
 export const key = ({key, metaKey, altKey}) => {
   const lastKey = localStorage.getItem('lastKey')
   const keyBinding = {
-    Enter: () => metaKey ? 'Meta + Enter' : altKey ? 'Alt + Enter' : 'Enter',
+    Enter:     () => metaKey ? 'Meta + Enter' : altKey ? 'Alt + Enter' : 'Enter',
     Backspace: () => 'Backspace',
-    ArrowUp: () => 'ArrowUp',
+    ArrowUp:   () => 'ArrowUp',
     ArrowDown: () => 'ArrowDown',
-    z: () => metaKey ? 'Meta + z' : 'z',
-    j: () => 'j',
-    k: () => 'k',
-    g: () => lastKey === 'g' ? 'gg' : 'g',
-    G: () => 'G',
-    d: () => lastKey === 'd' ? 'dd' : 'd',
-    u: () => 'u',
-    y: () => lastKey === 'y' ? 'yy' : 'y',
-    p: () => 'p',
+    z:         () => metaKey ? 'Meta + z' : 'z',
+    j:         () => 'j',
+    k:         () => 'k',
+    g:         () => lastKey === 'g' ? 'gg' : 'g',
+    G:         () => 'G',
+    d:         () => lastKey === 'd' ? 'dd' : 'd',
+    u:         () => 'u',
+    y:         () => lastKey === 'y' ? 'yy' : 'y',
+    p:         () => 'p',
   }[key]()
 
   localStorage.setItem('lastKey', keyBinding)
@@ -44,19 +44,19 @@ export const key = ({key, metaKey, altKey}) => {
 export const keyAction = event => {
   const {target} = event
   return {
-    Enter: () => action.open({target}),
+    Enter:          () => action.open({target}),
     'Meta + Enter': () => action.open({target, active: false}),
-    'Alt + Enter': () => action.open({target, currentTab: true}),
-    Backspace: () => action.remove(target),
-    'Meta + z': () => action.restore(),
-    ArrowUp: () => action.up(target),
-    ArrowDown: () => action.down(target),
-    j: () => action.down(target),
-    k: () => action.up(target),
-    gg: () => action.top(),
-    G: () => action.bottom(),
-    dd: () => action.remove(target),
-    u: () => action.restore(),
+    'Alt + Enter':  () => action.open({target, currentTab: true}),
+    Backspace:      () => action.remove(target),
+    'Meta + z':     () => action.restore(),
+    ArrowUp:        () => action.up(target),
+    ArrowDown:      () => action.down(target),
+    j:              () => action.down(target),
+    k:              () => action.up(target),
+    gg:             () => action.top(),
+    G:              () => action.bottom(),
+    dd:             () => action.remove(target),
+    u:              () => action.restore(),
   }[key(event)]()
 }
 
