40e6cb09a89937ef0feade745afb40455ec1a343
Will B Chang <will.b.chang@gmail.com>
2020-05-08 14:55:59 +0800

    refactor: move showDeleteIcon() and ShowFavIcon() to virtualDom.js

--- feat_popup/index.js
+++ feat_popup/index.js
@@ -13,23 +13,13 @@ import * as dispatch from './dispatch.js'
   pages.map(page => ul.append(html.renderListFrom(page)))
 
   // Listen mouse and keys events
-  ul.on({mouseenter: showDeleteIcon, mouseleave: showFavIcon}, 'img')
+  ul.on({
+    mouseenter: html.showDeleteIcon,
+    mouseleave: html.showFavIcon
+  }, 'img')
     .on('click', dispatch.click)
   $('body').on('keydown', dispatch.keydown)
 
   $('li')[0].focus()
 })()
-
-function showDeleteIcon(event) {
-  localStorage.setItem('src', event.target.src)
-  event.target.src = isDarkMode() ? '../assets/icons/delete-white32x32.png' : '../assets/icons/delete-black32x32.png'
-}
-
-function isDarkMode() {
-  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-}
-
-function showFavIcon(event) {
-  event.target.src = localStorage.getItem('src')
-}
 
