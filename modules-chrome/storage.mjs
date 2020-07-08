30550f2b7547a3192aa28531d7b6a508c8f97dd9
Will B Chang <will.b.chang@gmail.com>
2020-07-08 09:38:05 +0800

    style: move function

--- modules-chrome/storage.mjs
+++ modules-chrome/storage.mjs
@@ -2,13 +2,13 @@
 // https://developer.chrome.com/extensions/storage
 export const sync = {}
 
-sync.remove = url => chrome.storage.sync.remove(url)
-
 sync.get = () => new Promise(resolve => chrome.storage.sync.get(resolve))
 
 sync.set = page => new Promise(resolve =>
   chrome.storage.sync.set({[page.url]: page}, resolve)
 )
+
+sync.remove = url => chrome.storage.sync.remove(url)
 
 // NOTICE: This returns an Array of objects.
 sync.sortByLatest = async () => {
