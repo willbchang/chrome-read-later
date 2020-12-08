201c2f52425336785b0373081cd6707918a3ad63
Will B Chang <will.b.chang@gmail.com>
2020-12-08 16:51:41 +0800

    refactor(file structure): move feat-popup/* to core/popup/*

--- feat-popup/popup.js
+++
@@ -1,9 +0,0 @@
-import '../modules/prototype.mjs'
-import * as readingList from '../feature-reading-list/readingList.mjs'
-import * as statusBar from '../feature-status-bar/statusBar.js'
-
-$(async () => {
-  await readingList.setup()
-  statusBar.setup()
-})
-
