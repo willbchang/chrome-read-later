d09c4de642037a9fbd77d803fd6eb40ef9ff5c50
Will B Chang <will.b.chang@gmail.com>
2020-12-08 14:32:14 +0800

    refactor(file structure): extract folder feature-status-bar

--- feat-popup/index.js
+++ feat-popup/index.js
@@ -1,6 +1,6 @@
 import '../modules/prototype.mjs'
 import * as readingList from '../feature-reading-list/readingList.mjs'
-import * as statusBar from '../modules/statusBar.js'
+import * as statusBar from '../feature-status-bar/statusBar.js'
 
 $(async () => {
   await readingList.setup()
