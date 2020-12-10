330081f81575b325f812e1557fc44ebc65098214
Will B Chang <will.b.chang@gmail.com>
2020-12-10 16:55:36 +0800

    refactor: remove isHistoryPage()

--- features/history/history.js
+++ features/history/history.js
@@ -5,18 +5,9 @@ import * as statusBar from '../status-bar/statusBar.js'
 
 // Init history reading list from storage.
 $(async () => {
-  window.isHistoryPage = isHistoryPage()
+  window.isHistoryPage = true
   window.isHidingLi = false
   window.lastKey = ''
   await readingList.setup()
   statusBar.setup()
 })
-
-// Get the script file path and check if it's in history/
-function isHistoryPage() {
-  return $('script')
-    .filter((_, script) =>
-      script.src.includes('history/')
-    )
-    .length !== 0
-}
