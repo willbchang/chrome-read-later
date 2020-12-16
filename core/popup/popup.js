23182f8811572e2bfade89db43b315df11684b98
Will B Chang <will.b.chang@gmail.com>
2020-12-16 10:01:49 +0800

    refactor(reading-list): change *.mjs to *.js

--- core/popup/popup.js
+++ core/popup/popup.js
@@ -1,6 +1,6 @@
 import '../../modules/prototypes/Object.mjs'
 import '../../modules/prototypes/localStorage.mjs'
-import * as readingList from '../../features/reading-list/readingList.mjs'
+import * as readingList from '../../features/reading-list/readingList.js'
 import * as statusBar from '../../features/status-bar/statusBar.js'
 
 $(async () => {
