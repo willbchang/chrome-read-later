ba55e1d49f37ea75f3fc334f5c1413601fbeaa13
Will B Chang <will.b.chang@gmail.com>
2020-12-21 09:20:16 +0800

    refactor: extract links to constants

--- features/status-bar/statusBar.js
+++ features/status-bar/statusBar.js
@@ -8,12 +8,16 @@ export function setup() {
 }
 
 function init() {
+  const documentUrl = 'https://github.com/willbchang/chrome-read-later#features'
+  const feedbackUrl = 'https://github.com/willbchang/chrome-read-later/issues/new'
   $('#status-bar').append(`
      <li id="count"><span id="row">0</span>:<span id="total">0</span></li>
      <li id="history">History</li>
-     <li id="document"><a href="https://github.com/willbchang/chrome-read-later#features" target="_blank">Document</a>
+     <li id="document">
+        <a href="${documentUrl}" target="_blank">Document</a>
      </li>
-     <li id="feedback"><a href="https://github.com/willbchang/chrome-read-later/issues/new" target="_blank">Feedback</a>
+     <li id="feedback">
+        <a href="${feedbackUrl}" target="_blank">Feedback</a>
      </li>
   `)
 }
