6bce5ee3cad0c557cd314dcf705f54169d09de9c
Will B Chang <will.b.chang@gmail.com>
2020-12-22 12:15:46 +0800

    refactor: extract check is chrome favicon to String.prototype.isChromeFavicon

--- modules/prototypes/String.mjs
+++ modules/prototypes/String.mjs
@@ -1,6 +1,13 @@
 Object.defineProperty(String.prototype, 'isHttp', {
-  value: function () {
+  value:      function () {
     return this.slice(0, 4) === 'http'
+  },
+  enumerable: false
+})
+
+Object.defineProperty(String.prototype, 'isChromeFavicon', {
+  value:      function () {
+    return this.slice(0, 17) === 'chrome://favicon/'
   },
   enumerable: false
 })
