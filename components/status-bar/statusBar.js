e05edf14d758811aaa3eab8e96c54a73ed21020f
Will B Chang <will.b.chang@gmail.com>
2021-07-05 21:05:52 +0800

    ui: use question mark to replace history, document and feedback

--- components/status-bar/statusBar.js
+++ components/status-bar/statusBar.js
@@ -13,13 +13,7 @@ function init() {
   const feedbackUrl = 'https://github.com/willbchang/chrome-read-later/issues/new'
   $('#status-bar').append(`
      <li id="count" title="Row Number: Total Count"><span id="row">0</span>:<span id="total">0</span></li>
-     <li id="history" title="Reading List History">History</li>
-     <li id="document" title="${documentUrl}">
-        <a href="${documentUrl}" target="_blank">Document</a>
-     </li>
-     <li id="feedback" title="${feedbackUrl}">
-        <a href="${feedbackUrl}" target="_blank">Feedback</a>
-     </li>
+     <li id="feedback"><a href="https://github.com/willbchang/chrome-read-later#readme" target="_blank">?</a></li>
   `)
 }
 
