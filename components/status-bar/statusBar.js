5155d930d6b413825a1032392f79da45e99ef1cd
Will B Chang <will.b.chang@gmail.com>
2021-07-11 11:20:46 +0800

    ui: add history icon to status bar

--- components/status-bar/statusBar.js
+++ components/status-bar/statusBar.js
@@ -8,6 +8,9 @@ export function setup() {
 function init() {
   $('#status-bar').append(`
      <li id="count" title="Row Number: Total Count"><span id="row">0</span>:<span id="total">0</span></li>
+     <li id="history" title="Reading List History">
+        <img src="../../assets/icons/history.svg">
+     </li>
      <li id="info">
         <a href="https://github.com/willbchang/chrome-read-later#readme" target="_blank">
             <img src="../../assets/icons/info.svg"> 
