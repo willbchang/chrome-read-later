b7de7610238e7a77ead210b236fc4b96a3ccaeaa
Will B Chang <14329786+willbchang@users.noreply.github.com>
2022-01-22 23:00:47 +0800

    popup: change open behaviors based on options

--- popup/popup.js
+++ popup/popup.js
@@ -3,12 +3,15 @@ import '../modules/prototypes/localStorage.mjs'
 import * as readingList from './reading-list/readingList.js'
 import * as statusBar from './status-bar/statusBar.js'
 import * as runtime from '../modules/chrome/runtime.mjs'
+import * as storage from '../modules/chrome/storage.mjs'
 
 
 $(async () => {
     window.isHistory = false
     window.isHidingLi = false
     window.lastKey = ''
+    const {options} = await storage.sync.get('options')
+    window.options = options
     window.port = runtime.connect()
     await readingList.setup()
     statusBar.setup()
