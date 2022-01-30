e3e5df1970293c3fb7c813f1a4c52c45b721fc43
Will B Chang <14329786+willbchang@users.noreply.github.com>
2022-01-30 13:50:12 +0800

    fix: release doesn't contain options folder

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 mkdir ~/Desktop/chrome-read-later/
-cp -R icons background content popup modules manifest.json ~/Desktop/chrome-read-later
+cp -R icons background content popup options modules manifest.json ~/Desktop/chrome-read-later
 cd ~/Desktop
 zip -r ~/Desktop/chrome-read-later.zip ~/Desktop/chrome-read-later
