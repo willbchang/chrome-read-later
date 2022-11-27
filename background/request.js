075554a8c92794ab0c5cf70eddc2ff3a61d7f597
Will B Chang <will.b.chang@gmail.com>
2022-11-27 12:18:46 +0800

    dependency: remove jquery from background

--- background/request.js
+++ background/request.js
@@ -1,11 +1,13 @@
-import '../modules/libraries/jquery.min.js'
-
 export async function getTitle (url) {
     try {
         const response = await fetch(url)
         const html = await response.text()
-        return $(html).filter('title').text() || url
+        return parseTitle(html) || url
     } catch (e) {
         return url
     }
+}
+
+function parseTitle (html) {
+    return html.match(/<title>(.*?)<\/title>/)[1]
 }
