81bfc67b8482cdf41eda2726e4880dff9a8afa55
Will B Chang <will.b.chang@gmail.com>
2022-11-27 12:18:46 +0800

    popup(fix): no import error

--- popup/popup.js
+++ popup/popup.js
@@ -2,6 +2,7 @@ import '../modules/prototypes/Object.mjs'
 import * as readingList from './reading-list/readingList.js'
 import * as statusBar from './status-bar/statusBar.js'
 import * as runtime from '../modules/chrome/runtime.mjs'
+import * as storage from '../modules/chrome/storage.mjs'
 
 $(async () => {
     window.isHistory = false
