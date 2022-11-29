2f77f1ee088e9f98e04f6d1222844b5b1ae431d6
Will B Chang <will.b.chang@gmail.com>
2022-11-29 17:34:23 +0800

    background: delete sync urls first
      user always sees synced url, delete the removed url improve the UX

--- background/action.js
+++ background/action.js
@@ -44,10 +44,10 @@ export async function openPage ({ url, currentTab, active, isHistory }) {
 }
 
 export function removeDeletePages () {
-    localStore.getArray('deletedLocalUrls')
-        .then(data => data.map(url => storage.local.remove(url)))
-        .then(() => localStore.getArray('deletedSyncUrls'))
+    localStore.getArray('deletedSyncUrls')
         .then(data => data.map(url => storage.sync.remove(url)))
+        .then(() => localStore.getArray('deletedLocalUrls'))
+        .then(data => data.map(url => storage.local.remove(url)))
         .then(localStore.clear)
 
 }
