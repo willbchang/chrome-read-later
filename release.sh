fc454b4229b22aa43229876c64920b83afb8beb9
Will B Chang <will.b.chang@gmail.com>
2025-12-12 10:08:16 +0800

    chore: include `pages` directory in release script

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 mkdir ~/Desktop/chrome-read-later/
-cp -R icons background content popup options modules manifest.json ~/Desktop/chrome-read-later
+cp -R icons pages background content popup options modules manifest.json ~/Desktop/chrome-read-later
 cd ~/Desktop
 zip -r chrome-read-later.zip chrome-read-later
